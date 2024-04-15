60a78c95333d2a88b33ffcf56be1b4c8184faf49
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-15 19:43:43 +0000

    Added the StudentClass class with interfaces for the constructor and methods, allowing typed student management.

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,7 +15,9 @@ const teacher3: Teacher = {
   contract: false,
 };
 console.log(teacher3);
-interface Directors extends Teacher {
+
+
+export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
@@ -28,7 +30,8 @@ const director1: Directors = {
 };
 console.log(director1);
 
-interface printTeacherFunction {
+
+export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
@@ -36,3 +39,34 @@ const printTeacher: printTeacherFunction = function (firstName: string, lastName
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 console.log(printTeacher("Elon", "Musk"));
+
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+const student = new StudentClass("Bill", "Gates");
+console.log(student.displayName());
+console.log(student.workOnHomework());
