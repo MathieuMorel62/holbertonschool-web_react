e4a9265ba51e2974939739a375c6eac888d748b1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-18 17:50:08 +0000

    Update webpack config

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,30 +1,42 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
-  mode: 'production',
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-            },
+        type: 'asset/resource',
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65
+          },
+          optipng: {
+            enabled: false,
+          },
+          pngquant: {
+            quality: [0.65, 0.90],
+            speed: 4
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          webp: {
+            quality: 75
           }
-        ]
+        }
       }
-    ],
+    ]
   }
 };
