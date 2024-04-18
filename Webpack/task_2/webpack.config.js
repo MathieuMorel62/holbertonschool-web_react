32419cf7387d80994423f61a40781723d8390d49
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-18 17:54:00 +0000

    Update config to Webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -20,21 +20,10 @@ module.exports = {
         options: {
           mozjpeg: {
             progressive: true,
-            quality: 65
           },
           optipng: {
             enabled: false,
           },
-          pngquant: {
-            quality: [0.65, 0.90],
-            speed: 4
-          },
-          gifsicle: {
-            interlaced: false,
-          },
-          webp: {
-            quality: 75
-          }
         }
       }
     ]
