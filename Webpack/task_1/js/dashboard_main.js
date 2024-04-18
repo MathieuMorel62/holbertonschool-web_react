026387757ed174f1668c233267d39882f11ae3e9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-18 14:31:39 +0000

    Set up Webpack with jQuery and Lodash, added debounce functionality for button clicks.

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  function updateCounter() {
+    const count = parseInt($('#count').text()) || 0;
+    $('#count').text(`${++count} clicks on the button`);
+  }
+
+  $('startButton').on('click', _.debounce(updateCounter, 500));
+});
