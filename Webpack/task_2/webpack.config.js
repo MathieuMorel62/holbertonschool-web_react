709f78b845ee551cd1d67820ba16891ce110ade5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-18 16:53:40 +0000

    Add CSS and image handling to Webpack configuration

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65
+          },
+          optipng: {
+            enabled: false,
+          },
+          pngquant: {
+            quality: [0.65, 0.90],
+            speed: 4
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          webp: {
+            quality: 75
+          }
+        }
+      }
+    ]
+  }
+};
