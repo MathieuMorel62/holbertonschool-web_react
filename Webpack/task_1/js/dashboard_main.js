7e12ad29f2cd205db074ea47b5fcba3179383845
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-18 14:51:27 +0000

    change variable name

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -4,7 +4,7 @@ import { debounce } from 'lodash';
 $(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<button>Click here to get started</button>");
   $('body').append("<p id='count'></p>");
   $('body').append('<p>Copyright - Holberton School</p>');
 
@@ -14,5 +14,5 @@ $(document).ready(function() {
     $('#count').text(`${count} clicks on the button`);
   }
 
-  $('startButton').on('click', debounce(updateCounter, 500));
+  $('button').on('click', debounce(updateCounter, 500));
 });
