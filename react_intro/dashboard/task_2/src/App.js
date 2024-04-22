b5e7a1d410b260d99d5c646266127ec8a8c7d1b7
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-22 18:49:49 +0000

    Added login form with email and password fields and OK button

--- react_intro/dashboard/task_2/src/App.js
+++ react_intro/dashboard/task_2/src/App.js
@@ -13,7 +13,14 @@ function App() {
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+
+        <label htmlFor='email'>Email:</label>
+        <input type='email' id='email' name='email' />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' id='password' name='password' />
+        <button>OK</button>
       </div>
+
       <footer className="App-footer">
         <p>
           {getFullYear()} - {getFooterCopy(true)}
