a8b60cde9965999fd63443a315cbb0edc48ca26c
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-23 13:56:28 +0000

    Added form inputs and notification components with dynamic content and close functionality

--- react_intro/dashboard/task_2/src/Notifications.js
+++ react_intro/dashboard/task_2/src/Notifications.js
@@ -1,12 +1,38 @@
 import React from "react";
-import './Notifications.css';
+import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+
+import { getLatestNotification } from "./utils";
 
 function Notifications() {
   return (
     <div className="Notifications">
+      <button
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "20px",
+          border: "none",
+          background: "transparent",
+          cursor: "pointer",
+        }}
+        aria-label="Close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img
+          src={closeIcon}
+          alt="close button"
+          style={{ width: "12px", height: "12px" }}
+        />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li data-priority='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Notifications;
