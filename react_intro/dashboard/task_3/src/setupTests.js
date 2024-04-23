debe3c02f41b63a52972001ef1fffeb51d47662e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-23 16:30:24 +0000

    Add adaptater for use react test enzyme

--- react_intro/dashboard/task_3/src/setupTests.js
+++ react_intro/dashboard/task_3/src/setupTests.js
@@ -1,6 +1,4 @@
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { createSerializer } from 'enzyme-to-json';
+import { configure} from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
