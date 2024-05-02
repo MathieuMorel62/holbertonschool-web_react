99428723932f4056c72832102bc93e9ac0a414bf
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-02 09:39:45 +0000

    Converts the App functional component into a class component to align with the requirements of the React project.

---
+++ react_component/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CourseListRow from './CourseListRow';
+import './CourseList.css';
+import CourseShape from './CourseShape';
+
+
+const CourseList = ({ listCourses = [] }) => {
+  return (
+    <table id="CourseList">
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(course => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={String(course.credit)}
+              isHeader={false}
+            />
+          ))
+        ) : (
+          <tr>
+            <td colSpan="2">No course available yet</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
+};
+
+export default CourseList;
