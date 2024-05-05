cad7afe95d65d8a5e018bbcf8f4071983e10aefa
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-05 17:32:46 +0000

    Deleting a blank line

--- React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from "aphrodite";
 
-
 const headerStyle = { backgroundColor: '#deb5b545' };
 const rowStyle = { backgroundColor: '#f5f5f5ab' };
 
--- React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, css } from "aphrodite";
 
-
 const styles = StyleSheet.create({
   default: {
     color: "blue",
