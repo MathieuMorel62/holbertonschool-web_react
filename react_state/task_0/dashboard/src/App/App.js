8266001a76d7fa87a7e2a3a6bae8e1f296eb9baa
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-13 17:46:32 +0000

    Added displayDrawer state and handleDisplayDrawer, handleHideDrawer functions to manage notification drawer visibility.

---
+++ react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,125 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+import Footer from "../Footer/Footer";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+
+
+const style = StyleSheet.create({
+  app: {
+    fontFamily: 'Times New Roman, Times, serif',
+  },
+
+  body: {
+    height: '500px',
+  },
+
+  footer: {
+    display: 'flex',
+    position: 'fixed',
+    bottom: '0',
+    left: '0',
+    width: '100%',
+    textAlign: 'center',
+    padding: '10px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '1.2rem',
+  },
+});
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 }
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+      ],
+      displayDrawer: false,
+    };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { listCourses, listNotifications, displayDrawer } = this.state;
+
+    return (
+      <>
+        <Notifications displayDrawer={displayDrawer} listNotifications={listNotifications} handleDisplayDrawer={this.handleDisplayDrawer} handleHideDrawer={this.handleHideDrawer} />
+        <div className={css(style.app)}>
+          <div>
+            <Header />
+          </div>
+          <div className={css(style.body)}>
+            {isLoggedIn ?
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom> :
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>
+            }
+            <BodySection title="News from the School">
+              <p>Latest updates and insights from our school community.</p>
+            </BodySection>
+          </div>
+          <div className={css(style.footer)}>
+            <Footer />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
