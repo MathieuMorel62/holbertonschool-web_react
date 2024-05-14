6d94d4ce8025e0fb7c44ef0cbe0f6c138f1fe204
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-14 16:21:04 +0000

    Convert Header to class component and use context to display user info and logOut link

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { css, StyleSheet } from "aphrodite";
 import logo from "../assets/holberton-logo.jpg";
-
+import { AppContext } from "../App/AppContext";
 
 const styles = StyleSheet.create({
   header: {
@@ -10,38 +10,50 @@ const styles = StyleSheet.create({
     color: '#e11d3f',
     fontFamily: '"Times New Roman", Times, serif',
   },
-  
   h1: {
     margin: '0px',
     padding: '30px',
     flexGrow: '1',
     fontSize: '3rem',
   },
-  
   img: {
     maxHeight: '260px',
     marginRight: '1rem',
   },
-  
   hr: {
     color: 'red',
     backgroundColor: 'red',
     height: '0.1rem',
     minWidth: '100%',
   },
+  log: {
+    position: 'absolute',
+    right: '22px',
+    top: '14rem',
+    fontSize: '20px',
+  },
 });
 
+class Header extends Component {
+  static contextType = AppContext;
 
-const Header = () => {
-  return (
-    <>
-      <header className={css(styles.header)}>
-        <img src={logo} className={css(styles.img)} alt="Holberton Logo" />
-        <h1 className={css(styles.h1)}>School dashboard</h1>
-      </header>
-      <hr className={css(styles.hr)} />
-    </>
-  );
-};
+  render() {
+    const { user, logOut } = this.context;
+    return (
+      <>
+        <header className={css(styles.header)}>
+          <img src={logo} className={css(styles.img)} alt="Holberton Logo" />
+          <h1 className={css(styles.h1)}>School dashboard</h1>
+        </header>
+        {user.isLoggedIn && (
+          <section id="logoutSection">
+            <p className={css(styles.log)}>Welcome {user.email} (<a href="#logout" onClick={logOut}> logout </a>)</p>
+          </section>
+        )}
+        <hr className={css(styles.hr)} />
+      </>
+    );
+  }
+}
 
 export default Header;
