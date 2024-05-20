b6f4a347da0611f8dae590fd98fc96cca8a25667
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-20 11:03:51 +0000

    Set up schema using Normalizr and normalize notifications data

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,12 +1,11 @@
-import { normalize, schema } from 'normalizr';
-import * as notificationsData from '../../notifications.json';
+import { normalize, schema } from "normalizr";
+import * as notificationsData from "../../notifications.json";
 
+const user = new schema.Entity("users");
 
-const user = new schema.Entity('users');
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
 
-const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
-
-const notification = new schema.Entity('notifications', {
+const notification = new schema.Entity("notifications", {
   author: user,
   context: message,
 });
