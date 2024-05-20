bd107d1dded2d2b1afe6651fd66c0128f61ab188
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-20 13:50:13 +0000

    Add bound functions for course action creators

--- 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/courseActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/courseActionCreators.js
@@ -1,4 +1,5 @@
 import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { bindActionCreators } from "redux";
 
 
 export const selectCourse = (index) => {
@@ -13,4 +14,12 @@ export const unSelectCourse = (index) => {
     type: UNSELECT_COURSE,
     index,
   };
+}
+
+export const boundSelectCourse = (dispatch) => {
+  return bindActionCreators(selectCourse, dispatch);
+}
+
+export const boundUnSelectCourse = (dispatch) => {
+  return bindActionCreators(unSelectCourse, dispatch);
 }
