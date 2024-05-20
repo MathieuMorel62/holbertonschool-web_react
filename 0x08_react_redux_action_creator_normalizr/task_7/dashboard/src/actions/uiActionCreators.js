caacf23d62eb5a208de14d08544ac5280a2f835f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-20 18:05:36 +0000

    Add loginSuccess and loginFailure action creators and loginRequest async action

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,27 +1,61 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
+import { bindActionCreators } from 'redux';
 
 
-export const login = (email, password) => {
-  return {
-    type: LOGIN,
-    user: { email, password },
+export const login = (email, password) => ({
+  type: LOGIN,
+  user: { email, password }
+});
+
+export const logout = () => ({
+  type: LOGOUT
+});
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER
+});
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER
+});
+
+export const loginSuccess = () => ({
+  type: LOGIN_SUCCESS,
+});
+
+export const loginFailure = () => ({
+  type: LOGIN_FAILURE,
+});
+
+export const loginRequest = (email, password) => {
+  return async (dispatch) => {
+    dispatch(login(email, password));
+    try {
+      const response = await fetch('/login-success.json');
+      const data = await response.json();
+      dispatch(loginSuccess(data));
+    } catch (error) {
+      dispatch(loginFailure());
+    }
   };
+};
+
+export const boundLogin = (dispatch) => {
+  return bindActionCreators(login, dispatch);
 }
 
-export const logout = () => {
-  return {
-    type: LOGOUT,
-  };
+export const boundLogout = (dispatch) => {
+  return bindActionCreators(logout, dispatch);
 }
 
-export const displayNotificationDrawer = () => {
-  return {
-    type: DISPLAY_NOTIFICATION_DRAWER,
-  };
+export const boundDisplayNotificationDrawer = (dispatch) => {
+  return bindActionCreators(displayNotificationDrawer, dispatch);
 }
 
-export const hideNotificationDrawer = () => {
-  return {
-    type: HIDE_NOTIFICATION_DRAWER,
-  };
+export const boundHideNotificationDrawer = (dispatch) => {
+  return bindActionCreators(hideNotificationDrawer, dispatch);
+}
+
+export const boundLoginRequest = (dispatch) => {
+  return bindActionCreators(loginRequest, dispatch);
 }
