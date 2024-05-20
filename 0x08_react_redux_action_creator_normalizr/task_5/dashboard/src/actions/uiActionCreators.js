e268796acd52dacf1831c2b64087d30f22401376
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-20 13:50:54 +0000

    Add bound functions for UI action creators

--- 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,5 @@
 import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import { bindActionCreators } from "redux";
 
 
 export const login = (email, password) => {
@@ -24,4 +25,20 @@ export const hideNotificationDrawer = () => {
   return {
     type: HIDE_NOTIFICATION_DRAWER,
   };
+}
+
+export const boundLogin = (dispatch) => {
+  return bindActionCreators(login, dispatch);
+}
+
+export const boundLogout = (dispatch) => {
+  return bindActionCreators(logout, dispatch);
+}
+
+export const boundDisplayNotificationDrawer = (dispatch) => {
+  return bindActionCreators(displayNotificationDrawer, dispatch);
+}
+
+export const boundHideNotificationDrawer = (dispatch) => {
+  return bindActionCreators(hideNotificationDrawer, dispatch);
 }
