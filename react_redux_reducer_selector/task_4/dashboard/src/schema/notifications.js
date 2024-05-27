59340254a09f4838071233ee50462e19a2ae02ff
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-27 16:09:07 +0000

    Added the notification normalization function

--- react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -1,21 +1,19 @@
-import { normalize, schema } from "normalizr";
-import * as notificationsData from "../../notifications.json";
+import { normalize, schema } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
 
 
-const user = new schema.Entity("users");
-
-const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
-
-const notification = new schema.Entity("notifications", {
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
   author: user,
   context: message,
 });
 
+// Initial standardization of JSON file data
 const normalizedData = normalize(notificationsData.default, [notification]);
-
 export default normalizedData;
 
-
+// Function to get all notifications
 export function getAllNotificationsByUser(userId) {
   const notifications = normalizedData.entities.notifications;
   const messages = normalizedData.entities.messages;
@@ -23,4 +21,10 @@ export function getAllNotificationsByUser(userId) {
   return Object.values(notifications)
     .filter(notification => notification.author === userId)
     .map(notification => messages[notification.context]);
+}
+
+// Utility function to standardize new notification data
+export function notificationsNormalizer(data) {
+  if (!data) return { entities: { notifications: {} } };
+  return normalize(data, [notification]);
 }
