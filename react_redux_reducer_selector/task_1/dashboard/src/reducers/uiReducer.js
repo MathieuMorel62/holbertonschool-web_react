15bebc4133e55a9f80f61eb04e0c9edaef46920d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-27 12:05:05 +0000

    Using Immutable.js to manage the immutable state in the UI reducer

--- react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,3 +1,4 @@
+import { Map } from 'immutable';
 import {
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
@@ -7,42 +8,27 @@ import {
 } from '../actions/uiActionTypes';
 
 
-const initialState = {
+const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
   user: {}
-};
+});
+
 
 function uiReducer(state = initialState, action) {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: true
-      };
+      return state.set('isNotificationDrawerVisible', true);
     case HIDE_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: false
-      };
+      return state.set('isNotificationDrawerVisible', false);
     case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isUserLoggedIn: true
-      };
+      return state.set('isUserLoggedIn', true);
     case LOGIN_FAILURE:
-      return {
-        ...state,
-        isUserLoggedIn: false
-      };
     case LOGOUT:
-      return {
-        ...state,
-        isUserLoggedIn: false
-      };
+      return state.set('isUserLoggedIn', false);
     default:
       return state;
   }
-}
+};
 
 export default uiReducer;
