8b3b00477e2d3858bcffa5df9bde93aaf859a276
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-05-29 12:09:39 +0000

    Refactor Footer.test.js to use shallow and stateless component

--- react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { mount } from "enzyme";
-import Footer from "./Footer";
+import { shallow } from "enzyme";
+import { Footer } from "./Footer";
 import { StyleSheetTestUtils } from "aphrodite";
-import { AppContext, defaultUser } from "../App/AppContext";
+import { defaultUser } from "../App/AppContext";
 
 describe("Footer component", () => {
   beforeEach(() => {
@@ -14,39 +14,23 @@ describe("Footer component", () => {
   });
 
   it("renders without crashing", () => {
-    const wrapper = mount(
-      <AppContext.Provider value={{ user: defaultUser, logOut: jest.fn() }}>
-        <Footer />
-      </AppContext.Provider>
-    );
+    const wrapper = shallow(<Footer user={defaultUser} />);
     expect(wrapper.exists()).toBe(true);
   });
 
   it("renders a p tag with 'Copyright'", () => {
-    const wrapper = mount(
-      <AppContext.Provider value={{ user: defaultUser, logOut: jest.fn() }}>
-        <Footer />
-      </AppContext.Provider>
-    );
+    const wrapper = shallow(<Footer user={defaultUser} />);
     expect(wrapper.text()).toContain("Copyright");
   });
 
   it("does not display the Contact us link when user is logged out", () => {
-    const wrapper = mount(
-      <AppContext.Provider value={{ user: defaultUser, logOut: jest.fn() }}>
-        <Footer />
-      </AppContext.Provider>
-    );
+    const wrapper = shallow(<Footer user={defaultUser} />);
     expect(wrapper.find('a').exists()).toBe(false);
   });
 
   it("displays the Contact us link when user is logged in", () => {
     const loggedInUser = { email: 'test@example.com', password: 'password', isLoggedIn: true };
-    const wrapper = mount(
-      <AppContext.Provider value={{ user: loggedInUser, logOut: jest.fn() }}>
-        <Footer />
-      </AppContext.Provider>
-    );
+    const wrapper = shallow(<Footer user={loggedInUser} />);
     expect(wrapper.find('a').exists()).toBe(true);
     expect(wrapper.find('a').text()).toBe('Contact us');
   });
