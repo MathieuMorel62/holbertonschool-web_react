068d880862ec7d73fef762d927840a4cbca7e591
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-01 16:33:27 +0000

    Add SET_LOADING_STATE action type to notificationActionTypes.js

--- react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -6,3 +6,4 @@ export const NotificationTypeFilters = {
   URGENT: 'URGENT',
 }
 export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
+export const SET_LOADING_STATE = 'SET_LOADING_STATE';
