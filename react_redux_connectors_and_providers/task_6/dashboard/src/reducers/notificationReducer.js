5137700f3c3ded9d36d898d623902a0c229d630f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-01 18:28:38 +0000

    Update notificationReducer to handle loading state and SET_LOADING_STATE action

--- react_redux_connectors_and_providers/task_6/dashboard/src/reducers/notificationReducer.js
+++ react_redux_connectors_and_providers/task_6/dashboard/src/reducers/notificationReducer.js
@@ -15,7 +15,7 @@ const initialState = fromJS({
 function notificationReducer(state = initialState, action) {
   switch (action.type) {
     case MARK_AS_READ: {
-      const index = state.get('notifications').findIndex((notification) => notification.get('id') === action.index);
+      const index = state.get('notifications').findIndex((notification) => notification.get('id') === action.id);
       if (index !== -1) {
         return state.setIn(['notifications', index, 'isRead'], true);
       }
@@ -26,10 +26,10 @@ function notificationReducer(state = initialState, action) {
     case SET_LOADING_STATE:
       return state.set('loading', action.loading);
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return state.mergeDeep({
-        notifications: fromJS(action.data).map(notification => notification.set('isRead', false)),
-        loading: false,
-      });
+      return state.set('notifications', fromJS(action.data.map(notification => ({
+        ...notification,
+        isRead: false
+      }))));
     default:
       return state;
   }
