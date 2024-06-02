4854bcfa7b181c6a983dabb5b152d3d3055e660f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-02 21:28:27 +0000

    update selector tests and add test to verify unread urgent notifications when filter is set

--- react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.test.js
+++ react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.test.js
@@ -1,16 +1,16 @@
 import { fromJS } from 'immutable';
-import { filterTypeSelected, getNotifications, getUnreadNotifications } from './notificationSelector';
+import { filterTypeSelected, getNotifications, getUnreadNotificationsByType } from './notificationSelector';
 
 describe('notification selectors', () => {
   const state = fromJS({
-    filter: 'DEFAULT',
     notifications: {
+      filter: 'DEFAULT',
       notifications: [
-        { id: 1, type: 'default', value: 'New course available', isRead: false },
-        { id: 2, type: 'urgent', value: 'New resume available', isRead: true },
-        { id: 3, type: 'urgent', value: 'New data available', isRead: false }
-      ]
-    }
+        { id: 1, type: 'default', context: { value: 'New course available', isRead: false } },
+        { id: 2, type: 'urgent', context: { value: 'New resume available', isRead: true } },
+        { id: 3, type: 'urgent', context: { value: 'New data available', isRead: false } },
+      ],
+    },
   });
 
   it('filterTypeSelected works as expected', () => {
@@ -20,18 +20,25 @@ describe('notification selectors', () => {
   it('getNotifications returns the list of notifications', () => {
     const notifications = getNotifications(state);
     expect(notifications.toJS()).toEqual([
-      { id: 1, type: 'default', value: 'New course available', isRead: false },
-      { id: 2, type: 'urgent', value: 'New resume available', isRead: true },
-      { id: 3, type: 'urgent', value: 'New data available', isRead: false }
+      { id: 1, type: 'default', context: { value: 'New course available', isRead: false } },
+      { id: 2, type: 'urgent', context: { value: 'New resume available', isRead: true } },
+      { id: 3, type: 'urgent', context: { value: 'New data available', isRead: false } },
     ]);
   });
 
-  it('getUnreadNotifications returns the list of unread notifications', () => {
-    const unreadNotifications = getUnreadNotifications(state);
+  it('getUnreadNotificationsByType returns the list of unread notifications when filter is DEFAULT', () => {
+    const unreadNotifications = getUnreadNotificationsByType(state);
     expect(unreadNotifications).toEqual([
-      { id: 1, type: 'default', value: 'New course available', isRead: false },
-      { id: 3, type: 'urgent', value: 'New data available', isRead: false }
+      { id: 1, type: 'default', context: { value: 'New course available', isRead: false } },
+      { id: 3, type: 'urgent', context: { value: 'New data available', isRead: false } },
     ]);
   });
 
+  it('getUnreadNotificationsByType returns the list of unread urgent notifications when filter is URGENT', () => {
+    const stateWithUrgentFilter = state.setIn(['notifications', 'filter'], 'URGENT');
+    const unreadUrgentNotifications = getUnreadNotificationsByType(stateWithUrgentFilter);
+    expect(unreadUrgentNotifications).toEqual([
+      { id: 3, type: 'urgent', context: { value: 'New data available', isRead: false } },
+    ]);
+  });
 });
