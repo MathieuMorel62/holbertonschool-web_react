8a7049b02eee2e9b6e054707bb49d04661fd51e1
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-02 21:22:25 +0000

    add memoized selector getUnreadNotificationsByType with filtering by default and urgent types, remove getUnreadNotifications

--- react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
+++ react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
@@ -1,11 +1,20 @@
-import { fromJS } from 'immutable';
 import { createSelector } from 'reselect';
+import { fromJS } from 'immutable';
 
-export const filterTypeSelected = (state) => state.get('filter');
+export const filterTypeSelected = (state) => {
+  return state.getIn(['notifications', 'filter']);
+};
 
 export const getNotifications = (state) => state.getIn(['notifications', 'notifications'], fromJS([]));
 
-export const getUnreadNotifications = createSelector(
-  getNotifications,
-  (notifications) => notifications.toJS().filter(notification => !notification.isRead)
+export const getUnreadNotificationsByType = createSelector(
+  [filterTypeSelected, getNotifications],
+  (filter, notifications) => {
+    const unreadNotifications = notifications.toJS().filter(notification => notification.context && !notification.context.isRead);
+
+    if (filter === 'URGENT') {
+      return unreadNotifications.filter(notification => notification.type === 'urgent');
+    }
+    return unreadNotifications;
+  }
 );
