c67b069229edc1eb5b16583cb3ffca8dcbb16729
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-02 21:29:21 +0000

    Update notificationReducer to include context in MARK_AS_READ action

--- react_redux_connectors_and_providers/task_8/dashboard/src/reducers/notificationReducer.test.js
+++ react_redux_connectors_and_providers/task_8/dashboard/src/reducers/notificationReducer.test.js
@@ -3,26 +3,16 @@ import { fromJS } from 'immutable';
 import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER, SET_LOADING_STATE } from '../actions/notificationActionTypes';
 
 describe('notificationReducer', () => {
-  it('should return the initial state when no action is passed', () => {
-    const state = notificationReducer(undefined, {});
-    const expectedState = fromJS({
-      notifications: [],
-      filter: 'DEFAULT',
-      loading: false,
-    });
-    expect(state).toEqual(expectedState);
-  });
-
   it('should handle FETCH_NOTIFICATIONS_SUCCESS action', () => {
     const data = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', value: 'New data available' },
+      { id: 1, type: 'default', context: { value: 'New course available', isRead: false } },
+      { id: 2, type: 'urgent', context: { value: 'New resume available', isRead: false } },
+      { id: 3, type: 'urgent', context: { value: 'New data available', isRead: false } },
     ];
     const state = notificationReducer(undefined, { type: FETCH_NOTIFICATIONS_SUCCESS, data });
     const expectedState = fromJS({
       filter: 'DEFAULT',
-      notifications: data.map(notification => ({ ...notification, isRead: false })),
+      notifications: data,
       loading: false,
     });
     expect(state.toJS()).toEqual(expectedState.toJS());
@@ -31,14 +21,14 @@ describe('notificationReducer', () => {
   it('should handle MARK_AS_READ action', () => {
     const initialState = fromJS({
       notifications: [
-        { id: 1, type: 'default', value: 'New course available', isRead: false },
-        { id: 2, type: 'urgent', value: 'New resume available', isRead: false },
+        { id: 1, type: 'default', context: { value: 'New course available', isRead: false } },
+        { id: 2, type: 'urgent', context: { value: 'New resume available', isRead: false } },
       ],
       filter: 'DEFAULT',
       loading: false,
     });
     const state = notificationReducer(initialState, { type: MARK_AS_READ, id: 1 });
-    const expectedState = initialState.setIn(['notifications', 0, 'isRead'], true);
+    const expectedState = initialState.setIn(['notifications', 0, 'context', 'isRead'], true);
     expect(state).toEqual(expectedState);
   });
 
