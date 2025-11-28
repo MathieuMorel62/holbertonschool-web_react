4b0e49991a8cf601d0e1d403d106ce8f2a6d847f
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-11-28 19:21:16 +0100

    test

--- react_props/task_3/dashboard/src/App/App.jsx
+++ react_props/task_3/dashboard/src/App/App.jsx
@@ -1,39 +1,40 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import './App.css'
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import { getLatestNotification } from "../utils/utils";
 
 function App() {
   const notificationsList = [
     {
       id: 1,
-      type: 'default',
-      value: 'Notification available now'
+      type: "default",
+      value: "New course available"
     },
     {
       id: 2,
-      type: 'urgent',
-      value: 'Notification available now'
+      type: "urgent",
+      value: "New resume available"
     },
     {
       id: 3,
-      type: 'urgent',
-      html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' }
+      type: "urgent",
+      value: getLatestNotification()
     }
   ];
 
   return (
-    <Fragment>
-      <div className="root-notifications">
-        <Notifications notifications={notificationsList} />
-      </div>
+    <>
+      <Notifications notifications={notificationsList} />
+
       <Header />
+
       <Login />
+
       <Footer />
-    </Fragment>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
