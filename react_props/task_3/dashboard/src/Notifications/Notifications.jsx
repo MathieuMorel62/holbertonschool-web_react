835b74cf68b2d141a86c6cef39de89cb7221183a
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-11-28 19:46:35 +0100

    fgdf

--- react_props/task_3/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_3/dashboard/src/Notifications/Notifications.jsx
@@ -1,31 +1,39 @@
-import './Notifications.css';
-import closeIcon from '../assets/close-icon.png';
-import NotificationItem from './NotificationItem';
+import React from "react";
+import "./Notifications.css";
+import NotificationItem from "./NotificationItem";
+import closeIcon from "../assets/close-icon.png";
 
-export default function Notifications({ notifications = [] }) {
+import { getLatestNotification } from "../utils/utils";
+
+function Notifications() {
   return (
-    <div className='notification-items'>
-      <p>Here is the list of notifications</p>
-      <ul>
-        {notifications.map((notification) => (
-          <NotificationItem
-            key={notification.id}
-            type={notification.type}
-            value={notification.value}
-            html={notification.html}
-          />
-        ))}
-      </ul>
+    <div className="Notifications">
       <button
-        aria-label='Close'
-        type='button'
-        onClick={() => console.log('Close button has been clicked')}
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "20px",
+          border: "none",
+          background: "transparent",
+          cursor: "pointer",
+        }}
+        aria-label="Close"
+        onClick={() => console.log("Close button has been clicked")}
       >
         <img
-          alt='close-icon'
           src={closeIcon}
+          alt="close button"
+          style={{ width: "12px", height: "12px" }}
         />
       </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem type="urgent" html={{ __html: getLatestNotification() }}></NotificationItem>
+      </ul>
     </div>
   );
 }
+
+export default Notifications;
