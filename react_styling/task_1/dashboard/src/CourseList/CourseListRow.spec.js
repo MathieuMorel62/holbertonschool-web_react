1ed8dfb3bec390ed647eb0a4479ccd09aaa7ffd7
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-02-11 22:06:54 +0100

    test

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.spec.js
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.spec.js
@@ -3,38 +3,20 @@ import { render, screen } from '@testing-library/react';
 
 describe('course list row tests', () => {
   test('when isHeader is true and textSecondCell is null, renders one th with colspan=2', () => {
-    render(
-      <table>
-        <thead>
-          <CourseListRow isHeader={true} textSecondCell={null} />
-        </thead>
-      </table>
-    );
+    render(<CourseListRow isHeader={true} textSecondCell={null} />);
     const header = screen.getByRole('columnheader');
     expect(header).toBeInTheDocument();
     expect(header).toHaveAttribute('colspan', '2');
   });
 
   test('when isHeader is true and textSecondCell not null, redner twh th', () => {
-    render(
-      <table>
-        <thead>
-          <CourseListRow isHeader={true} textSecondCell={'Bonjour'} />
-        </thead>
-      </table>
-    );
+    render(<CourseListRow isHeader={true} textSecondCell={'Bonjour'} />);
     const headers = screen.getAllByRole('columnheader');
     expect(headers).toHaveLength(2);
   });
 
   test('when isHeader false, renders two td within a tr', () => {
-    render(
-      <table>
-        <tbody>
-          <CourseListRow isHeader={false} />
-        </tbody>
-      </table>
-    );
+    render(<CourseListRow isHeader={false} />);
     const row = screen.getByRole('row');
     expect(row).toBeInTheDocument();
     const cells = screen.getAllByRole('cell');
