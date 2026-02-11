908637832f9845dbd708a5593fcefb7be73627c5
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-02-11 22:08:15 +0100

    test

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -5,38 +5,33 @@ function CourseList({ courses = [] }) {
   return (
     <div className="courses-container w-4/5 mx-auto">
       <table id="coursesTable" className="w-full border-collapse">
-        {courses.length === 0 ? (
-          <thead>
+        <thead>
+          <CourseListRow textFirstCell="Available courses" isHeader={true} />
+          <CourseListRow
+            textFirstCell="Course name"
+            textSecondCell="Credit"
+            isHeader={true}
+          />
+        </thead>
+
+        <tbody>
+          {courses.length === 0 ? (
             <CourseListRow
               textFirstCell="No course available yet"
-              isHeader={true}
+              isHeader={false}
+              textSecondCell={null}
             />
-          </thead>
-        ) : (
-          <>
-            <thead>
+          ) : (
+            courses.map((course) => (
               <CourseListRow
-                textFirstCell="Available courses"
-                isHeader={true}
+                key={course.id}
+                textFirstCell={course.name}
+                textSecondCell={course.credit}
+                isHeader={false}
               />
-              <CourseListRow
-                textFirstCell="Course name"
-                textSecondCell="Credit"
-                isHeader={true}
-              />
-            </thead>
-            <tbody>
-              {courses.map((course) => (
-                <CourseListRow
-                  key={course.id}
-                  textFirstCell={course.name}
-                  textSecondCell={course.credit}
-                  isHeader={false}
-                />
-              ))}
-            </tbody>
-          </>
-        )}
+            ))
+          )}
+        </tbody>
       </table>
     </div>
   );
