1bf9ce3bad1d7690c5c01214818c48a78d75efd3
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-02-11 17:05:41 +0100

    test

--- react_styling/task_1/dashboard/src/main.jsx
+++ react_styling/task_1/dashboard/src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App/App.jsx';
+import './main.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App  isLoggedIn={true}/>
+  </StrictMode>,
 );
-
-reportWebVitals();
